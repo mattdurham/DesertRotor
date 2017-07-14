324ca01eecba2bfe9a2977cf5dfc9dc51f5b9007
Alexander <axolson@gmail.com>
2017-07-14 16:55:59 -0700

    Importing Project

---
+++ DesertRotor/Controllers/ContactRepository_contactRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DesertRotor.Controllers
+{
+    internal class ContactRepository_contactRepository
+    {
+        internal static void AddContact(object contact)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DesertRotor/Controllers/HomeController.cs
@@ -0,0 +1,69 @@
+﻿using DesertRotor.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DesertRotor.Controllers
+{
+    public class HomeController : Controller
+    {
+         static ContactRepository_contactRepository = new ContactRepository();
+;        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult CreateContact()
+        {
+            return View("AddContact");
+        }
+
+        public ActionResult AddContact(Contact contact)
+        {
+                _contactRepository.AddContact(contact);
+                return View("SingleContact", contact);
+        }
+
+        
+
+        public ActionResult ViewContacts()
+        {
+            return View("ContactList", _contactRepository.Contacts);
+        }
+    }
+
+    public class ContactRepository
+    {
+        private List<Contact> _contacts = new List<Contact>();
+
+        public void AddContact(Contact contact)
+        {
+            _contacts.Add(contact);
+        }
+
+        public List<Contact> Contacts
+        {
+            get
+            {
+                return _contacts;
+            }
+        }
+
+    }
+}
